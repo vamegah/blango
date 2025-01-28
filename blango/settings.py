7d55c9f5617a85482b0309667921e9f323a2cede
victor Amega <vamega13@gmail.com>
2025-01-28 20:20:12 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -53,6 +53,7 @@ class Dev(Configuration):
       'blog',
       'crispy_forms',
       "crispy_bootstrap5",
+      'debug_toolbar',
     
   ]
 
@@ -92,6 +93,7 @@ class Dev(Configuration):
 
 
   MIDDLEWARE = [
+      "debug_toolbar.middleware.DebugToolbarMiddleware",
       'django.middleware.security.SecurityMiddleware',
       'django.contrib.sessions.middleware.SessionMiddleware',
       'django.middleware.common.CommonMiddleware',
@@ -100,7 +102,7 @@ class Dev(Configuration):
       'django.contrib.messages.middleware.MessageMiddleware',
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
   ]
-  INTERNAL_IPS = ["192.168.10.226"]
+  INTERNAL_IPS = ["192.168.11.179"]
 
   ROOT_URLCONF = 'blango.urls'
 
