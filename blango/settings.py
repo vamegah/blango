b2b809f82302624ef3c1c6071689539ac4adcdbf
victor Amega <vamega13@gmail.com>
2025-01-29 04:06:38 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -21,6 +21,9 @@ class Dev(Configuration):
   # Build paths inside the project like this: BASE_DIR / 'subdir'.
   BASE_DIR = Path(__file__).resolve().parent.parent
 
+  EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+  ACCOUNT_ACTIVATION_DAYS = 7
+
 
   # Quick-start development settings - unsuitable for production
   # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
--- blango/urls.py
+++ blango/urls.py
@@ -17,7 +17,9 @@ import debug_toolbar
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-
+import blango_auth.views
+from django_registration.backends.activation.views import RegistrationView
+from blango_auth.forms import BlangoRegistrationForm
 import blog.views
 
 urlpatterns = [
@@ -25,6 +27,14 @@ urlpatterns = [
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/profile/", blango_auth.views.profile, name="profile"),
+    path(
+      "accounts/register/",
+    RegistrationView.as_view(form_class=BlangoRegistrationForm),
+    name="django_registration_register",),
+    path("accounts/", include("django_registration.backends.activation.urls")),
+
 ]
 if settings.DEBUG:
     urlpatterns += [
